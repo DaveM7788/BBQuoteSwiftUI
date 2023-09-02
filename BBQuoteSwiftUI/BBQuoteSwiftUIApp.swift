d549de64f4cc518bc3a76024888ccfa0f76a77f4
DaveM7788 <david.mcdermott98@gmail.com>
2023-09-01 20:17:46 -0500

    Initial Commit

---
+++ BBQuoteSwiftUI/BBQuoteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuoteSwiftUIApp.swift
+//  BBQuoteSwiftUI
+//
+//  Created by David McDermott on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BBQuoteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
